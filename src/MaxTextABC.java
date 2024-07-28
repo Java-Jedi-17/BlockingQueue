f83d471b08aefab5adc2d2645addd37d046c643e
JavaJedi <aleksk170400@gmail.com>
2024-07-29 02:07:01 +0500

    Initial commit

--- src/MaxTextABC.java
+++ src/MaxTextABC.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class MaxTextABC {
+public class MaxTextABC<T, I> {
+
+    protected T text;
+    protected I count;
+
+    public void setText(T text) {
+        this.text = text;
+    }
+
+    public void setCount(I count) {
+        this.count = count;
+    }
+
+    public T getText() {
+        return text;
+    }
+
+    public I getCount() {
+        return count;
+    }
 }
